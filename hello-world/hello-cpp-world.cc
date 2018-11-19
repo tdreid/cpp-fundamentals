527b4186f9c225ba55342cc418cd79c5733ba75d
Trevor Reid <trevor.d.reid@gmail.com>
2018-11-19 22:40:52 +0000

    Initial commit

---
+++ hello-world/hello-cpp-world.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Type your first name" << std::endl;
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name << std::endl;
+    
+    return 0;
+}
